955cd9ca42b4f86c34e8e2db72453de20c573716
Aaryav1130 <146359088+Aaryav1130@users.noreply.github.com>
2025-12-28 22:10:30 +0530

    Remove userId dependency, use only deviceId for preferences - Updated schema and API endpoints

---
+++ migrations/development/relations.ts
@@ -0,0 +1,14 @@
+
+import { relations } from "drizzle-orm/relations";
+import { bookCacheInDevelopment, savedBooksInDevelopment } from "./schema";
+
+export const savedBooksInDevelopmentRelations = relations(savedBooksInDevelopment, ({one}) => ({
+	bookCacheInDevelopment: one(bookCacheInDevelopment, {
+		fields: [savedBooksInDevelopment.bookCacheId],
+		references: [bookCacheInDevelopment.id]
+	}),
+}));
+
+export const bookCacheInDevelopmentRelations = relations(bookCacheInDevelopment, ({many}) => ({
+	savedBooksInDevelopments: many(savedBooksInDevelopment),
+}));
